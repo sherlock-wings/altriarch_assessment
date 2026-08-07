52e9c8d707a1055f8cfd93a9dd0688a2bc023bae
Patrick F Callahan <pfc1047@proton.me>
2026-08-06 22:43:07 -0400

    moving source data for pt2

--- pt2/01-data-load.sql
+++ pt2/01-data-load.sql
@@ -7,3 +7,7 @@ type = filefmt_csv
 field_delimiter = ','
 field_optionally_enclosed_by = '"'
 skip_header = 1;
+
+create stage if not exists internal_stage_csv
+file_format = filefmt_csv
+directory = (enable = true);
