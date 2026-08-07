2df5fd4d0c989c54d88c450804cd43ace625d392
Patrick F Callahan <pfc1047@proton.me>
2026-08-07 10:33:59 -0400

    better name

--- pt2/04-03-clean-transactions.sql
+++ pt2/04-03-clean-transactions.sql
@@ -5,7 +5,7 @@ create or replace transient table int_transactions (
 transaction_sk varchar, 
 transaction_id varchar,
 transaction_date date,
-fund varchar,
+fund_description varchar,
 share_class varchar,
 facility_sk varchar,
 transaction_type varchar,
@@ -16,7 +16,7 @@ insert into int_transactions
 select md5(nvl(transaction_id::varchar, 'NULL')) as transaction_sk
       ,transaction_id
       ,to_date(transaction_date, 'dd-mon-yy') as transaction_date
-      ,fund
+      ,fund as fund_description
       ,share_class
       ,md5(nvl(investment_ref::varchar, 'NULL')) as facility_sk
       ,transaction_type
