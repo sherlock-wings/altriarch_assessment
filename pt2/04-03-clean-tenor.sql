e9d91230e75b60acca5165014cc868c83b06c374
Patrick F Callahan <pfc1047@proton.me>
2026-08-07 02:00:27 -0400

    better name

--- pt2/04-03-clean-tenor.sql
+++ pt2/04-03-clean-tenor.sql
@@ -1,4 +1,17 @@
 use role candidate_callahan;
 use schema callahan_db.staging;
 
-select * from callahan_db.raw.
+select md5(transaction_id::varchar, 'NULL') as transaction_sk
+      ,transaction_id
+      ,to_date(transaction_date, 'dd-mon-yy') as transaction_date
+      ,fund
+      ,share_class
+      ,investment_ref
+      ,transaction_type
+      ,case 
+         when regexp_like(amount, '^\\(.*\\)$')
+         then regexp_replace(amount, ',|\\(|\\)', '')::number(35,3)*-1
+         else regexp_replace(amount, ',|\\(|\\)', '')::number(35,3)
+       end as amount
+from callahan_db.raw.tenor_transactions_export
+;
