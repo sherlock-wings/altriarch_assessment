664e16e389eb2d8090f513171b56956d290b9e93
Patrick F Callahan <pfc1047@proton.me>
2026-08-08 16:37:01 -0400

    some comments explaining rationale

--- pt2/03_marts/04-correct-nfe-dim-facility.sql
+++ pt2/03_marts/04-correct-nfe-dim-facility.sql
@@ -1,6 +1,30 @@
 use role candidate_callahan;
 use schema callahan_db.marts;
 
+/*
+DATA CONFLICT
+
+On: NET_FUNDS_EMPLOYED
+
+Between: FACTORVIEW system and TENOR system
+
+Resolution: Defer to TENOR's FACILITY_BALANCE field unless the facility in question
+            is not found in the Tenor data
+
+Rationale: 
+  - The fund descriptions between Tenor and Factorview differ only in a superficial sense,
+    (e.g. 'Cardinal ABL & Factoring Fund' from Factorview and 'ABL Fund' from Tenor), so
+    it seems likely that these systems are in fact referring to the same fund and just 
+    using different verbiage
+ 
+  - Which fund is tied to which facility should be most authoratatively determined by the 
+    Loan Servicing platform, since that's where the source-of-truth for all loans (and 
+    the funds they are tied to) lives. 
+
+  - The fund descriptions from Factorview are also generally more detailed than the 
+    ones from Tenor
+*/
+
 update dim_facility tgt 
 set tgt.net_funds_employed = src.facility_balance
 from (
