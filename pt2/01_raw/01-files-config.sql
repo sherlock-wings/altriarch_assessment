28d7ed054ec1b1ce6f0f50d37cbd1e9392dc252f
Patrick F Callahan <pfc1047@proton.me>
2026-08-08 19:15:03 -0400

    taking a break

--- pt2/01_raw/01-files-config.sql
+++ pt2/01_raw/01-files-config.sql
@@ -2,7 +2,7 @@ use role candidate_callahan;
 use schema callahan_db.raw;
 
 create or replace file format filefmt_csv
-type = filefmt_csv
+type = CSV
 field_delimiter = ','
 field_optionally_enclosed_by = '"'
 skip_header = 1;
